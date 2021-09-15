1e4576bc55914b57285e9e31b2ae539ed554fa87
Keaton <keaton.arneson@gmail.com>
2021-09-15 12:41:34 -0500

    Sidebar creation with dummy data

---
+++ src/components/Sidebar.js
@@ -0,0 +1,34 @@
+import '../App.css';
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar-container">
+      <h3>Contacts</h3>
+
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+        <li>Item 5</li>
+        <li>Item 6</li>
+        <li>Item 7</li>
+        <li>Item 8</li>
+        <li>Item 9</li>
+        <li>Item 10</li>
+        <li>Item 11</li>
+        <li>Item 12</li>
+        <li>Item 13</li>
+        <li>Item 14</li>
+        <li>Item 15</li>
+        <li>Item 16</li>
+        <li>Item 17</li>
+        <li>Item 18</li>
+        <li>Item 19</li>
+        <li>Item 20</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Sidebar;
