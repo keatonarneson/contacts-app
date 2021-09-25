8b544d395c5651415399da7dcfa3116005557251
Keaton <keaton.arneson@gmail.com>
2021-09-25 14:27:13 -0500

    Added core functionality to all buttons and forms

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,24 +1,36 @@
-import { useContext, useEffect, useState } from 'react';
+import { useContext, useEffect } from 'react';
 import { GlobalContext } from '../context/GlobalState';
 
-import Email from './EmailItem';
+import EmailItem from './EmailItem';
 import EmailForm from './EmailForm';
+import Alert from './Alert';
 
-const Main = () => {
-  const [valueFirst, setValueFirst] = useState('');
-  const [valueLast, setValueLast] = useState('');
-  const [valueEmail, setValueEmail] = useState('');
+const Main = ({
+  valueFirst,
+  setValueFirst,
+  valueLast,
+  setValueLast,
+  emailArray,
+  setEmailArray,
+  valueEmail,
+  setValueEmail,
+  setActiveIndex,
+}) => {
   const {
     selectedContact,
-    contacts,
+    setSelectedContact,
     getContacts,
     showEmailForm,
     toggleShowEmailForm,
-    setTempContact,
+    addContact,
+    updateContact,
+    deleteContact,
+    message,
   } = useContext(GlobalContext);
 
   useEffect(() => {
     getContacts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
@@ -26,7 +38,13 @@ const Main = () => {
       setValueFirst(selectedContact.firstName);
       setValueLast(selectedContact.lastName);
     }
-  }, [selectedContact]);
+  }, [selectedContact, setValueFirst, setValueLast]);
+
+  useEffect(() => {
+    if (selectedContact.emails) {
+      setEmailArray(selectedContact.emails);
+    }
+  }, [selectedContact, setEmailArray]);
 
   const handleChangeFirst = e => {
     setValueFirst(e.target.value);
@@ -40,33 +58,73 @@ const Main = () => {
     toggleShowEmailForm(showEmailForm);
   };
 
-  console.log(setTempContact);
+  const handleSave = () => {
+    let contact = {
+      firstName: valueFirst,
+      lastName: valueLast,
+      emails: emailArray,
+    };
+    if (Object.keys(selectedContact).length !== 0) {
+      contact = { ...contact, id: selectedContact.id };
+      updateContact(contact);
+    } else {
+      addContact(contact);
+    }
+    setValueEmail('');
+    setEmailArray([]);
+    setValueFirst('');
+    setValueLast('');
+    setSelectedContact({});
+    setActiveIndex(-1);
+
+    getContacts();
+  };
+
+  const handleDelete = e => {
+    deleteContact(selectedContact);
+    setValueEmail('');
+    setEmailArray([]);
+    setValueFirst('');
+    setValueLast('');
+    setActiveIndex(-1);
+  };
+
+  const handleCancel = () => {
+    setValueEmail('');
+    setEmailArray([]);
+    setValueFirst('');
+    setValueLast('');
+    setSelectedContact({});
+    setActiveIndex(-1);
+  };
 
   return (
     <div className="main-container">
-      <div className="alert-container"></div>
+      <div className="alert-container">{message && <Alert />}</div>
 
       <div className="form-container">
         <form>
-          <label for="first-name">First Name</label>
+          <label htmlFor="first-name">First Name</label>
           <br />
           <input
             type="text"
             id="first-name"
             name="first-name"
             placeholder="First Name"
+            required
             value={valueFirst}
             onChange={handleChangeFirst}
           />
         </form>
         <form>
-          <label for="last-name">Last Name</label>
+          <label htmlFor="last-name">Last Name</label>
           <br />
           <input
             type="text"
             id="last-name"
             name="last-name"
             placeholder="Last Name"
+            required
             value={valueLast}
             onChange={handleChangeLast}
           />
@@ -78,8 +136,16 @@ const Main = () => {
       <div className="email-container">
         <h6>Email</h6>
         <ul>
-          {selectedContact.emails && <Email emails={selectedContact.emails} />}
+          {emailArray?.map((email, i) => (
+            <EmailItem
+              key={i}
+              email={email}
+              emailArray={emailArray}
+              setEmailArray={setEmailArray}
+            />
+          ))}
         </ul>
+
         <div className="add-email-container">
           <button onClick={handleEmailForm} className="circle email-add">
             <div className="bar horizontal" />
@@ -90,17 +156,48 @@ const Main = () => {
         </div>
         <EmailForm
           showEmailForm={showEmailForm}
-          value={valueEmail}
-          setValue={setValueEmail}
-          // setTempContactEmail={setTempContact.emails}
+          formValue={valueEmail}
+          setFormValue={setValueEmail}
+          emailArray={emailArray}
+          setEmailArray={setEmailArray}
         />
       </div>
 
       <div className="button-container">
-        <button id="delete-btn">Delete</button>
+        <button
+          id="delete-btn"
+          disabled={Object.keys(selectedContact).length === 0}
+          onClick={() => {
+            const confirmBox = window.confirm(
+              'Do you really want to delete this contact?'
+            );
+            if (confirmBox === true) {
+              handleDelete();
+            }
+          }}
+        >
+          Delete
+        </button>
         <div className="button-group">
-          <button id="cancel-btn">Cancel</button>
-          <button id="save-btn">Save</button>
+          <button
+            id="cancel-btn"
+            onClick={handleCancel}
+            disabled={
+              valueLast <= 0 &&
+              valueFirst <= 0 &&
+              valueEmail <= 0 &&
+              emailArray.length <= 0
+            }
+          >
+            Cancel
+          </button>
+          <button
+            id="save-btn"
+            onClick={handleSave}
+            disabled={valueFirst <= 0 || valueLast <= 0}
+          >
+            Save
+          </button>
         </div>
       </div>
     </div>
