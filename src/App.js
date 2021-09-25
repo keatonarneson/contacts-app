7799d23e601fcee3e840d325129cfef611c4268c
Keaton <keaton.arneson@gmail.com>
2021-09-25 14:22:41 -0500

    Moved local state to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
 import './App.css';
+import { useState } from 'react';
 import Sidebar from './components/Sidebar';
 import Main from './components/Main';
 
 import { GlobalProvider } from './context/GlobalState';
 
 const App = () => {
+  const [valueFirst, setValueFirst] = useState('');
+  const [valueLast, setValueLast] = useState('');
+  const [emailArray, setEmailArray] = useState([]);
+  const [valueEmail, setValueEmail] = useState('');
+  const [activeIndex, setActiveIndex] = useState(-1);
   return (
     <GlobalProvider>
       <div className="app">
-        <Sidebar />
-        <Main />
+        <Sidebar
+          valueFirst={valueFirst}
+          setValueFirst={setValueFirst}
+          valueLast={valueLast}
+          setValueLast={setValueLast}
+          emailArray={emailArray}
+          setEmailArray={setEmailArray}
+          valueEmail={valueEmail}
+          setValueEmail={setValueEmail}
+          activeIndex={activeIndex}
+          setActiveIndex={setActiveIndex}
+        />
+        <Main
+          valueFirst={valueFirst}
+          setValueFirst={setValueFirst}
+          valueLast={valueLast}
+          setValueLast={setValueLast}
+          emailArray={emailArray}
+          setEmailArray={setEmailArray}
+          valueEmail={valueEmail}
+          setValueEmail={setValueEmail}
+          setActiveIndex={setActiveIndex}
+        />
       </div>
     </GlobalProvider>
   );
