d1b437633029b4496252e860491f01663c9b0699
Keaton <keaton.arneson@gmail.com>
2021-09-25 14:26:53 -0500

    Refactored contacts list

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,33 +1,54 @@
-import { useContext, useEffect } from 'react';
+import { useContext } from 'react';
 import { GlobalContext } from '../context/GlobalState';
 
-import Contact from './ContactItem';
+import ContactItem from './ContactItem';
 
-const Sidebar = () => {
-  const { contacts, getContacts } = useContext(GlobalContext);
+const Sidebar = ({
+  setValueFirst,
+  setValueLast,
+  setEmailArray,
+  setValueEmail,
+  activeIndex,
+  setActiveIndex,
+}) => {
+  const { contacts, setSelectedContact, loading } = useContext(GlobalContext);
 
-  useEffect(() => {
-    getContacts();
-  }, []);
-
-  const clickHandler = e => {};
+  const clickHandler = () => {
+    setValueEmail('');
+    setEmailArray([]);
+    setValueFirst('');
+    setValueLast('');
+    setSelectedContact({});
+    setActiveIndex(-1);
+  };
 
   return (
-    <div className="sidebar-container">
+    <div className="sidebar-container scroll-bar">
       <div className="contacts-header-container">
         <h3>Contacts</h3>
 
-        <button className="circle contacts-add">
+        <button className="circle contacts-add" onClick={clickHandler}>
           <div className="bar horizontal" />
           <div className="bar vertical" />
         </button>
       </div>
 
-      <ul>
-        {contacts.map(contact => (
-          <Contact key={contact.id} contact={contact} />
-        ))}
-      </ul>
+      {loading ? (
+        <div className="loader" />
+      ) : (
+        <ul>
+          {contacts &&
+            contacts.map((contact, index) => (
+              <ContactItem
+                key={index}
+                contact={contact}
+                activeIndex={activeIndex}
+                setActiveIndex={setActiveIndex}
+                index={index}
+              />
+            ))}
+        </ul>
+      )}
     </div>
   );
 };
